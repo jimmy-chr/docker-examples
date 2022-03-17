a1c54c2b53ff30aebf50d09aff6944cb432c4f98
jimmy-chr <48068249+jimmy-chr@users.noreply.github.com>
2022-03-17 14:50:41 +0100

    Add files via upload

--- user-service-api/index.js
+++ user-service-api/index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.json([{ name: "Bob", email: "bob@bob.com" }]);
+  res.json([
+    { name: "Bob", email: "bob@bob.com" },
+    { name: "Alice", email: "alice@alice.com" },
+    { name: "Jake", email: "jake@jake.com" },
+    { name: "Maria", email: "maria@maria.com" },
+  ]);
 });
 
 app.listen(port, () => {
